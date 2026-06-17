d26c4ccb0e8cc3eb00d99fb77f3482f6338efd16
RobLoweMOJ <robert.lowe@digital.justice.gov.uk>
2026-06-17 13:37:26 +0100

    Add Webpack fix and reapply tidy up

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,10 +9,6 @@ mix
     .js('src/js/admin/index.js', 'dist/admin.min.js')
     .js('src/js/login.js', 'dist/js/login.min.js')
     .js('src/js/script-localization.js', 'dist/script-localization.min.js')
-    /** patch code for CCFW **/
-    .js('src/js/patch/ccfw-cookie-manage.js', 'dist/patch/js/ccfw-cookie-manage.js')
-    .js('src/js/patch/ccfw-frontend.js', 'dist/patch/js/ccfw-frontend.js')
-    /** -------------- **/
     .sass('src/sass/app.scss', 'dist/css/app.min.css')
     .sass('src/sass/admin.scss', 'dist/css/admin.min.css')
     .sass('src/sass/editor.scss', 'dist/css/editor.min.css')
